33775826b51bf0e9a698ac27359e79a03cf2f2e5
tsoumagas-benjamin <bptsoumagas@gmail.com>
2024-11-25 18:42:23 -0500

    Completed requirements for Lab 8 Part 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Created a student object with a method to retrieve relevant information
 const student = {
-    name: "Ben",
+    studentName: "Ben",
     age: 23,
     enrolled: true,
     courses: ["Algorithms", "Databases", "JavaScript", "Operating Systems", "Technical Writing", "Web Programming"],
@@ -17,7 +17,7 @@ const student = {
 }
 
 // Part 1
-console.log(student.name);
+console.log(student.studentName);
 console.log(student.age);
 console.log(student.getInfo());
 
@@ -28,3 +28,14 @@ console.log(studentJSON);
 // Turn the JSON back to an object and log it to the console
 let studentObject = JSON.parse(studentJSON);
 console.log(studentObject);
+
+// Part 3
+// Destructure the object to get name and courses
+const { studentName, age, enrolled, courses} = student;
+console.log(studentName);
+console.log(courses);
+// Create an array of scores 
+const scores = [83, 45, 28, 91, 82];
+const [first, second] = scores;
+console.log(first);
+console.log(second);
